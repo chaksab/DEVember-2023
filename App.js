272a0dd1e2ed911d5950f1512d408f220ad1b15d
chaksab <schakra300888@gmail.com>
2024-01-20 10:59:42 +0530

    Added Boxes and styling

--- App.js
+++ App.js
@@ -4,7 +4,15 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <View style={styles.box}>
+        <Text style={styles.text}>1</Text>
+      </View>
+
+      <View style={styles.box}>
+        <Text style={styles.text}>2</Text>
+      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +24,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 10
+
   },
+  box: {
+    backgroundColor: '#F9EDE3',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 10,
+    borderColor: '#9B4521',
+    width: 300,
+    height: 200,
+  },
+  text: {
+    color: '#9B4521',
+    fontSize: 70
+  }
 });
